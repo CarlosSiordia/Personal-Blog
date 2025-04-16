a3d655642183b63f8e216629d24eead9cb907a30
CarlosSiordia <cf0810003@gmail.com>
2025-04-16 17:41:28 -0600

    I start development of page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://CarlosSiordia.github.io',
-  base: 'Personal-Blog',
-
   vite: {
     plugins: [tailwindcss()],
   },
